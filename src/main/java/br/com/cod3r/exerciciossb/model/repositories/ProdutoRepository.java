87b5216ba2d33f9da9c35d349e23168dbd33a6a5
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-02 17:57:09 -0300

    Metodos.

--- src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,9 +1,21 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
 
     public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+
+    // findByNomeContaining
+    // findByNomeIsContaining
+    // findByNomeContains
+
+    // findByNomeStartsWith
+    // findByNomeEndsWith
+
+    // findByNomeNotContaining
+
 }
