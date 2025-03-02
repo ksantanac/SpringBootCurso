011ac88d247ca562827e62888783802e433f0080
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-02 17:35:47 -0300

    Add GET finding by part of name.

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -4,6 +4,8 @@ import br.com.cod3r.exerciciossb.model.entities.Produto;
 import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -26,7 +28,10 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
-
+    @GetMapping(path = "/nome/{parteNome}")
+    public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+        return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+    }
 
     @GetMapping(path = "/{id}")
     public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
--- src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
 
-
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
